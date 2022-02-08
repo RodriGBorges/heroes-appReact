bfc59fe23eb664ac9547b0d90d5ff8039730a513
RodriGBorges <gabii.borges1998@gmail.com>
2022-02-08 15:41:57 -0300

    Componente lista de heroes

--- src/Components/HeroesList.js
+++ src/Components/HeroesList.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import { getHeroesByPublisher } from '../Selectors/getHeroesByPublisher';
+import { HeroeCard } from './HeroeCard';
 
-export const HeroesList = () => {
-    return <div></div>;
+export const HeroesList = ({publisher}) => {
+
+    const heroes = getHeroesByPublisher(publisher);
+
+    return (
+        <>
+            {
+                heroes.map(heroe => (
+                    <HeroeCard
+                        key={heroe.id}
+                        {...heroe}
+                    />
+                ))
+            }
+        </>
+    );
 };
