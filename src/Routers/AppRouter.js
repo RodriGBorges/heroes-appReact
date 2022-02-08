41d31f49b5e674694aa24818e7e0ed76b695739f
RodriGBorges <gabii.borges1998@gmail.com>
2022-02-08 20:03:38 -0300

    Validacion simple con navigate

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { AuthContext } from '../Auth/AuthContext';
 import { Login } from '../Pages/Login';
 import { DashboardRouter } from './DashboardRouter';
 
 
 export const AppRouter = () => {
+
+    const { user } = useContext(AuthContext);
+    const estaLogueado = user.logueado;
+    // estaLogueado/ 2 valores / true o false
+
     return (
         <BrowserRouter>
             <div className='container'>
                 <Routes>
                     <Route path='/login' element={<Login/>} />
-                    <Route path='*' element={<DashboardRouter/>} />
+                    <Route path='*' element={ estaLogueado ? <DashboardRouter/> : <Navigate to='/login'/>} />
                 </Routes>
             </div>
         </BrowserRouter>
