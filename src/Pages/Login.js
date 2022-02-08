3dee1f066b7bb6c163df74b999490bfce76cdb23
RodriGBorges <gabii.borges1998@gmail.com>
2022-02-08 19:47:17 -0300

    Logeo de usuario

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Auth/AuthContext';
+import { types } from '../Types/Index';
 
 export const Login = () => {
+    
+    const {dispatch} = useContext(AuthContext);
+
+    const navigate = useNavigate()
+    //cuando se logee, automaticamente te devuelve a la pagina anterior. 
+
+    const handleLogin = () => {
+
+        dispatch({
+            type: types.login,
+            payload: {
+                name: 'Rodrigo'
+            }
+        })
+
+        navigate('/')
+        //o -1 -2 para volver páginas atras
+
+    }
+
     return (
-        <div>
+        <div className='mt-5'>
             <h1>Login</h1>
             <hr/>
+            <button className='btn btn-primary' onClick={handleLogin}>
+                Login
+            </button>
         </div>
     );
 };
